3861a2b62ab91ca886293abeb751fbcccdfc8c55
kevinpeuman <kevinpeuman@gmail.com>
2020-06-26 14:25:55 +0200

    Update index.php
    PHP 7.4 - Unparenthesized `a ? b : c ? d : e` is deprecated. Use either `(a ? b : c) ? d : e` or `a ? b : (c ? d : e)

--- payment/index.php
+++ payment/index.php
@@ -247,7 +247,7 @@ if (count($_POST) > 0) {
 //
 function getURL()
 {
-    $s        = empty($_SERVER["HTTPS"]) ? '' : ($_SERVER["HTTPS"] == "on") ? "s" : "";
+    $s        = empty($_SERVER["HTTPS"]) ? '' : (($_SERVER["HTTPS"] == "on") ? "s" : "");
     $sp       = strtolower($_SERVER["SERVER_PROTOCOL"]);
     $protocol = substr($sp, 0, strpos($sp, "/")) . $s;
     return $protocol . "://" . $_SERVER['SERVER_NAME'] . dirname($_SERVER['PHP_SELF']);
