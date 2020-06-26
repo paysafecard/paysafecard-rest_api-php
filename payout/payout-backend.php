a8883aa4cf4b7fcbd696795c54dd456007bc0e5f
kevinpeuman <kevinpeuman@gmail.com>
2020-06-26 14:27:47 +0200

    Update payout-backend.php
    PHP 7.4 - Unparenthesized `a ? b : c ? d : e` is deprecated. Use either `(a ? b : c) ? d : e` or `a ? b : (c ? d : e)

--- payout/payout-backend.php
+++ payout/payout-backend.php
@@ -318,7 +318,7 @@ if (count($payouts) > 0) {
 // get the current URL
 function getURL()
 {
-    $s        = empty($_SERVER["HTTPS"]) ? '' : ($_SERVER["HTTPS"] == "on") ? "s" : "";
+    $s        = empty($_SERVER["HTTPS"]) ? '' : (($_SERVER["HTTPS"] == "on") ? "s" : "");
     $sp       = strtolower($_SERVER["SERVER_PROTOCOL"]);
     $protocol = substr($sp, 0, strpos($sp, "/")) . $s;
     return $protocol . "://" . $_SERVER['SERVER_NAME'] . dirname($_SERVER['PHP_SELF']);
